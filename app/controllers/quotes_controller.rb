60b6cec6e15df81a5430f2321d2b34cb98e8bd1c
Ben Plunkert <bplunkert@Bens-MacBook-Air.local>
2018-05-12 23:04:31 -0400

    initial commit

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,74 @@
+class QuotesController < ApplicationController
+  before_action :set_quote, only: [:show, :edit, :update, :destroy]
+
+  # GET /quotes
+  # GET /quotes.json
+  def index
+    @quotes = Quote.page params[:page]
+  end
+
+  # GET /quotes/1
+  # GET /quotes/1.json
+  def show
+  end
+
+  # GET /quotes/new
+  def new
+    @quote = Quote.new
+  end
+
+  # GET /browse
+  # GET /browse.json
+  def browse
+    @quotes = Quote.all
+  end
+
+  # GET /random
+  # GET /random.json
+  def random
+    quote_ids = 25.times.map{ rand(1..Quote.count) }
+    @quotes = quote_ids.map{|id| Quote.exists?(id) && Quote.find(id) }.select{|quote| quote unless quote.nil?}
+  end
+
+  # POST /quotes
+  # POST /quotes.json
+  def create
+    @quote = Quote.new(quote_params)
+
+    respond_to do |format|
+      if @quote.score != 0 or @quote.approved
+        format.html { render :new }
+        format.json { render json: @quote.errors, status: :unprocessable_entity }
+      end
+
+      if @quote.save
+        format.html { redirect_to @quote, notice: 'Quote was successfully created.' }
+        format.json { render :show, status: :created, location: @quote }
+      else
+        format.html { render :new }
+        format.json { render json: @quote.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /quotes/1
+  # DELETE /quotes/1.json
+  def destroy
+    @quote.destroy
+    respond_to do |format|
+      format.html { redirect_to quotes_url, notice: 'Quote was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_quote
+      @quote = Quote.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def quote_params
+      params.require(:quote).permit(:text, :score)
+    end
+end
