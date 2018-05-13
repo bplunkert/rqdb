256eb5ac9e0ae438b2ac3361ae2faad0a005eb07
Ben Plunkert <bplunkert@Bens-MacBook-Air.local>
2018-05-12 23:27:56 -0400

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :quotes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/', to: :index, controller: 'quotes'
-  get 'browse', to: :index, controller: 'quotes'
+  #get '/', to: :index, controller: 'quotes'
+  #get 'browse', to: :index, controller: 'quotes'
   get 'latest', to: :latest, controller: 'quotes'
   get 'random', to: :random, controller: 'quotes'
 
