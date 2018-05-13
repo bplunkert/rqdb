d0a1305db913e3503214ec175dd6acf134a6175b
Ben Plunkert <bplunkert@Bens-MacBook-Air.local>
2018-05-13 00:24:02 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'browse', to: 'quotes#index'
-  get 'latest', to: 'quotes#latest'
-  get 'random', to: :random, controller: 'quotes'
+  get 'browse',   to: 'quotes#index'
+  get 'latest',   to: 'quotes#latest'
+  get 'random',   to: 'quotes#random'
+  get 'random1',  to: 'quotes#random1'
+  get 'search',   to: 'quotes#search'
+  get 'top',      to: 'quotes#top'
 
 end
