93abf17ca68a5d4c9b43a943ef9df1ab0c856bc5
Ben Plunkert <bplunkert@Bens-Air.netgear.com>
2018-05-13 22:52:45 -0400

    add flagged column to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_12_222706) do
+ActiveRecord::Schema.define(version: 2018_05_14_024535) do
 
   create_table "quotes", force: :cascade do |t|
     t.string "text", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_05_12_222706) do
     t.boolean "approved", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "flagged"
   end
 
 end
