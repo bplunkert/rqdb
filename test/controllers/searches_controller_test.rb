a9736ced34f13db40c3ed397e92387acc474d545
Ben Plunkert <bplunkert@bplunkert>
2018-05-21 09:46:28 -0400

    fix test

--- test/controllers/searches_controller_test.rb
+++ test/controllers/searches_controller_test.rb
@@ -6,20 +6,20 @@ class SearchesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "search should return matching quotes" do
-    post '/search', params: { search: { search_pattern: 'MyString1' } }
+    post '/search', params: { pattern: 'MyString1' }
     assert_response :success
     assert_select "pre", {:class => "quote_output", :count=>1}
 
   end
 
   test "search should not return nonmatching quotes" do
-    post '/search', params: { search: { search_pattern: 'BogusStringWithNoMatchingQuotes' } }
+    post '/search', params: { pattern: 'BogusStringWithNoMatchingQuotes' }
     assert_response :success
     assert_select "pre", {:class => "quote_output", :count=>0}
   end
 
   test "empty search pattern should not return quotes" do
-    post '/search', params: { search: { search_pattern: '' } }
+    post '/search', params: { pattern: '' }
     assert_response :success
     assert_select "pre", {:class => "quote_output", :count=>0}
   end
