498da5b5b0d24be729eac029e12f1cd53d66e2a8
Ben Plunkert <bplunkert@bplunkert>
2018-05-22 10:21:42 -0400

    set announcement text column to not null

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_22_041222) do
+ActiveRecord::Schema.define(version: 2018_05_22_142058) do
 
   create_table "announcements", force: :cascade do |t|
-    t.string "text"
+    t.string "text", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
