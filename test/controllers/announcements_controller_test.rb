ddd56256731d962ace2afe6e1a73ec80b3d88ae4
Ben Plunkert <bplunkert@bplunkert>
2018-05-22 08:50:33 -0400

    add AnnouncementsController test for index

--- test/controllers/announcements_controller_test.rb
+++ test/controllers/announcements_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class AnnouncementsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @announcements = announcements
+    @announcement  = announcements(:one)
+  end
+
+  test 'should get index' do
+    get '/'
+    assert_response :success
+  end
 end
