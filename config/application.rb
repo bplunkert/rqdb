9b3f00a54e27c6d29fe5df87fa9fbc785e83162e
Ben Plunkert <bplunkert@users.noreply.github.com>
2019-01-05 17:03:47 -0500

    Revert "Release 0.1.1 (#3)"
    This reverts commit 331b67843ffcf8ba0abdde2e53534ef5d4ced51c.

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,6 @@ module RashOnRails
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.version = "0.1.1"
+    config.version = "0.1"
   end
 end
