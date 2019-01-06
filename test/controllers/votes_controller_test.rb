29ecc222280b26a56da0c072c440f1a1dfcbabfc
Ben Plunkert <bplunkert@Bens-MacBook-Pro.local>
2019-01-06 10:40:59 -0500

    split votes controller tests to separate file votes_controller_test.rb

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class VotesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @quote = quotes(:one)
+  end
+
+  test 'user should upvote quote only once' do
+    assert_difference('Quote.find(@quote.id).score', +1) do
+      3.times do
+        get "/quotes/#{@quote.id}/upvote"
+      end
+    end
+    assert_redirected_to quote_url(@quote)
+  end
+
+  test 'user should downvote quote only once' do
+    assert_difference('Quote.find(@quote.id).score', -1) do
+      3.times do
+        get "/quotes/#{@quote.id}/downvote"
+      end
+    end
+    assert_redirected_to quote_url(@quote)
+  end
 end
