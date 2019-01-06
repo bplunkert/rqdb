a87422cb314af0a6a105d5919896fcd279dafee6
Ben Plunkert <bplunkert@Bens-MacBook-Pro.local>
2019-01-06 10:53:46 -0500

    fix issue #6 broken goto quote_id search box in navbar

--- test/controllers/searches_controller_test.rb
+++ test/controllers/searches_controller_test.rb
@@ -6,20 +6,33 @@ class SearchesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'search should return matching quotes' do
-    post '/search', params: { pattern: 'MyString1' }
+    post search_url, params: { pattern: 'MyString2' }
     assert_response :success
-    assert_select 'pre.quote_output', {:count=>1}
+    assert_select 'pre', {:class => 'quote_output', :count=>1}    
+    assert_match /MyString2/, response.parsed_body, 'Search failed when passing valid quote_id parameter'
   end
 
   test 'search should not return nonmatching quotes' do
-    post '/search', params: { pattern: 'BogusStringWithNoMatchingQuotes' }
+    post search_url, params: { pattern: 'BogusStringWithNoMatchingQuotes' }
     assert_response :success
-    assert_select 'pre.quote_output', {:count=>0}
+    assert_select 'pre', {:class => 'quote_output', :count=>0}
   end
 
   test 'empty search pattern should not return quotes' do
-    post '/search', params: { pattern: '' }
+    post search_url, params: { pattern: '' }
     assert_response :success
-    assert_select 'pre.quote_output', {:count=>0}
+    assert_select 'pre', {:class => 'quote_output', :count=>0}
+  end
+
+  test 'search with valid quote_id should return only one quote' do
+    post search_url, params: { quote_id: 2 }
+    assert_response :success
+    assert_select 'pre', {:class => 'quote_output', :count=>1}
+  end
+
+  test 'search with invalid quote_id should return no quotes' do
+    post search_url, params: { quote_id: 999999999999 }
+    assert_response :success
+    assert_select 'pre', {:class => 'quote_output', :count=>0}
   end
 end
