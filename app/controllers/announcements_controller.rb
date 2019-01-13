e6a68b69474f19540733116f6b495325c3b16567
Ben Plunkert <bplunkert@Bens-MacBook-Pro.local>
2019-01-12 19:43:15 -0500

    fix comment text in announcements_controller

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -8,8 +8,8 @@ class AnnouncementsController < ApplicationController
     @announcements = Announcement.order(created_at: :desc).page(params[:page])
   end
 
-  # GET /quotes/1
-  # GET /quotes/1.json
+  # GET /announcements/1
+  # GET /announcements/1.json
   def show
     @announcements = [@announcement]
   end
