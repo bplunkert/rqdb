60871c74e3c034e90f9f1f4c82beeb4861e11acc
Ben Plunkert <bplunkert@Bens-MacBook-Pro.local>
2019-03-23 12:25:04 -0400

    update version to 0.3.1

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,6 @@ module RashOnRails
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.version = "0.3.0"
+    config.version = "0.3.1"
   end
 end
